3cbbbb1a2115439a7c5ec57ad904129652927433
千帆过尽 <bken2016@163.com>
2017-12-28 21:22:49 +0800

    Create clone.js

---
+++ clone.js
@@ -0,0 +1,18 @@
+function clone(obj) {
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+
+  let toStr = Object.prototype.toString;
+  let newObj = {};
+  let isArray = toStr.call(obj) === '[object Array]';
+  
+  if (isArray) {
+    newObj = [];
+    Object.keys(obj).map(key => newObj.push(clone(obj[key])));
+  } else {
+    Object.keys(obj).map(key => newObj[key] = clone(obj[key]));
+  }
+
+  return newObj;
+}
