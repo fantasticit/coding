8b2b497aca76141bccdc689b574bab2eb14b2a57
tingyun <tingyun@pinduoduo.com>
2020-10-23 22:14:01 +0800

    feat: update dep version

--- docs/config.js
+++ docs/config.js
@@ -33,5 +33,18 @@ export default {
       title: "Github",
       path: "https://github.com/fantasticit/coding"
     }
-  ]
+  ],
+  manifest: { 
+    "name" : "Fantasticit: 学习笔记" , 
+    "short_name" : "Fantasticit" , 
+    "display" : "standalone" , 
+    "start_url" : "/" , 
+    "theme_color" : "#000" , 
+    "background_color" : "#000" , 
+    "icons" : [72, 96, 120, 128, 144, 152, 180, 192, 384, 512].map(s => ({
+      "src": `assets/icon${s}.png`,
+      "sizes": `${s}x${s}`,
+      "type": "image/png"
+    }))
+  }
 }
